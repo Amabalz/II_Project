8370a7e223c491eb5fef87669214421874ca010e
Petcut Adrian <iq2adi@gmail.com>
2024-04-14 14:26:24 +0300

    only one controller

---
+++ Project_II.Server/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Project_II.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+    namespace Project_II.Server.Data
+    {
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+            {
+            }
+
+            public DbSet<User> UserData { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<User>().HasKey(u => u.id);
+            }
+        }
+    }
---
+++ Project_II.Server/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Project_II.Server.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+        public string profile_picture { get; set; }
+        public int level {  get; set; }
+
+    }
+}
